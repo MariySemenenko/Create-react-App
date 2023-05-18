8e79f49f2bf3f63d3a2384231daa619268476ddc
MariySemenenko <mashyni92@gmail.com>
2023-05-18 19:31:01 +0300

    props

--- my-app-ui/src/components/productsDetails/Productsdetails.js
+++ my-app-ui/src/components/productsDetails/Productsdetails.js
@@ -1,11 +1,32 @@
 
-function ProductsDateils () {
+import PropTypes from 'prop-types'; //щоб стандартизувати наші пропси
 
+function ProductsDateils ({name, type, price}) {
+// const productName = props.name;
+const totalPrice = price * 1.2;
+
+if (!name || !type) {
+    return (
+        <span>Product is empty</span>
+    )
+}
+ if (!price) {
+    return '';
+ }
     return (
         <div>
-            I am products and i have dateils!;
+           <>I am products.</> 
+           <>My name is: {name}. </> 
+           <>My name is type: {type}. </>
+           <>This price: {totalPrice}. </>
         </div>
     )
 }
+
+ProductsDateils.propTypes = {
+    name: PropTypes.string,
+    type: PropTypes.string,
+    price: PropTypes.number.isRequired,
+  };
 
 export default ProductsDateils;
--- my-app-ui/src/components/productslist/Productslist.js
+++ my-app-ui/src/components/productslist/Productslist.js
@@ -2,13 +2,38 @@ import ProductsDateils from "../productsDetails/Productsdetails";
  
 const ProductsList = () => {
 const text = 'I am productsList';
+
+const products = [
+    {id: '3', name: 'orange', type: 'apple', price: 100},
+    {id: '1', name: 'milk', type: 'chery', price: 5},
+    {id: '8', name: 'bread', type: 'banana', price: 10},
+    {id: '5', name: 'cucumber', type: 'cocos', price: 150},
+
+]
+
+
+
     return (
 <>
-{text}
-<ProductsDateils />
-<ProductsDateils />
-<ProductsDateils />
-<ProductsDateils />
+<div>{text}</div>
+<ul>
+    {products.map((product) => (
+        <li>
+            <ProductsDateils
+            key={product.id}
+            name={product.name}
+            type={product.type}
+            price={product.price}
+            />
+        </li>
+    ))}
+</ul>
+
+{/* 
+<ProductsDateils name='milk'  />
+<ProductsDateils name='bread' type='2' price={100} />
+<ProductsDateils name='chocolad' type='3' price='200' />
+<ProductsDateils name='energy drink' type='4' price='150' /> */}
 </>
     )
 }
